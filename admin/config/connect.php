a7bc2e4bc1390e51fab0b096cb89f090a4a63527
HoaZinhh <hoavuongvinh@gmail.com>
2023-03-07 19:54:21 +0700

    1.2
    Config

--- admin/config/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost","root","","webshoe-mysqli");
-
-// Check connection
-if ($mysqli->connect_error) {
-  echo "Kết nối MYSQLI lỗi" . $mysqli->connect_error;
-  exit();
-}
-?>
